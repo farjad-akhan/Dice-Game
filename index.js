3ac5620250e2fc27b66b3f5b5828fadd38b5101e
farjad-akhan <73795065+farjad-akhan@users.noreply.github.com>
2021-08-26 16:15:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomNumber2= Math.floor(Math.random()*6)+1;
+
+var randomDiceImage1="images/dice"+randomNumber1+".png";
+var randomDiceImage2="images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage1)
+document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2)
+
+if(randomNumber1>randomNumber2){
+  document.querySelector("h1").innerHTML="🚩 Player 1 Won";
+}else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="Player 2 Won🚩";
+}else{
+  document.querySelector("h1").innerHTML="Draw!";
+}
